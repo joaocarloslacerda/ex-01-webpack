b43a1c940cfd9ae9cad1707923181debf864682f
joão <juniorlacerdasilva@gmail.com>
2024-05-16 23:50:24 -0300

    Adicionado alert de operação inválida

--- divisao.js
+++ divisao.js
@@ -1,7 +1,11 @@
+import { validaDivisao } from "./validaCalculo" 
+
 export function dividirDoisNumeros(){
 
     const valorUm = Number(document.getElementById("numeroUm").value)
     const valorDois = Number(document.getElementById("numeroDois").value)
+
+    validaDivisao(valorUm, valorDois)
 
     const valorResultado = document.getElementById("valorResultado")
 
---
+++ validaCalculo.js
@@ -0,0 +1,6 @@
+export function validaDivisao(valorUm, valorDois){
+
+    if(valorUm <= 0 || valorDois <= 0){
+        alert("OPERAÇÃO INVÁLIDA\n\nNão é possível dividir por número menor ou igual a 0")
+    }
+}
